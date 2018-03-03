0e16c552a023d519feeccb46dc1fa86c8a6f6473
qinjunshan <13782533791@163.com>
2018-03-03 22:37:09 +0800

    add

--- jQueryAjax/ajax-0.1.js
+++ jQueryAjax/ajax-0.1.js
@@ -1,5 +1,22 @@
 (function (global, factory) {
     global.$ = factory();
 })(this, function () {
-    
+    var jQuery = function () {
+        return new jQuery.fn.init();
+    };
+
+    jQuery.prototype = {
+        init: function () {
+
+        },
+        ajax: function () {
+
+        }
+    };
+    jquery.extend = jQuery.prototype.extend = function () {
+
+    };
+
+    jQuery.fn = jQuery.prototype;
+    return jquery;
 });
